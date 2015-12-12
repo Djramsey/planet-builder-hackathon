0bee914ab9c85f37b1757f144084715ba9662ea0
Divy Tolia <divystolia@gmail.com>
2015-12-12 18:26:30 +0000

    planet name working

--- form.js
+++ form.js
@@ -1,15 +1,21 @@
+/* Control Planet Appearance */
 function updatePlanetColor(){
-    document.getElementById('theplanet').style.background = document.getElementById('planetcolor').value
+    document.getElementById('theplanet').style.background = document.getElementById('planetcolor').value;
+}
+
+function updatePlanetName(){
+    document.getElementById('planetname').innerHTML = document.getElementById('planetnameinput').value;
 }
 
 
 function updatePlanetSize(){
-    var size = $('#size').val()
-    $('#theplanet').width(size)
-    $('#theplanet').height(size)
+    var size = $('#size').val();
+    $('#theplanet').width(size);
+    $('#theplanet').height(size);
 }
 
 $( '#build' ).click(function() {
-  updatePlanetColor()
-  updatePlanetSize()
+  updatePlanetColor();
+  updatePlanetSize();
+  updatePlanetName();
 });
