9c0db0f013bf0c083551df7fc0ace6f707b73b0b
Divy Tolia <divystolia@gmail.com>
2015-12-12 21:30:33 +0000

    trying to restore sounds

--- form.js
+++ form.js
@@ -1,17 +1,16 @@
 /* Control Planet Appearance */
 function updatePlanetColor(){
-    document.getElementById('theplanet').style.background = document.getElementById('planetcolor').value;
+  document.getElementById('theplanet').style.background = document.getElementById('planetcolor').value;
 }
 
 function updatePlanetName(){
-    document.getElementById('planetname').innerHTML = document.getElementById('planetnameinput').value;
+  document.getElementById('planetname').innerHTML = document.getElementById('planetnameinput').value;
 }
-
 
 function updatePlanetSize(){
-    var size = $('#size').val();
-    $('#theplanet').width(size);
-    $('#theplanet').height(size);
+  var size = $('#size').val();
+  $('#theplanet').width(size);
+  $('#theplanet').height(size);
 }
 
 $( '#build' ).click(function() {
@@ -20,43 +19,43 @@ $( '#build' ).click(function() {
   updatePlanetName();
 });
 
-$( '#build' ).click(function() {
-  updatePlanetColor();
-  updatePlanetSize();
-  updatePlanetName();
-});
 
 $( '#moon-add' ).click(function() {
-    $('.moon').toggle()
+  $('.moon').toggle()
 });
 
 $( '#ring-add' ).click(function() {
-    $('#planetrings').toggle()
+  $('#planetrings').toggle()
 });
 
 $( '#rocket' ).click(function() {
-    $('#rocket-box').show()
-    $('#rocket-box').slideUp(2000)
-    collision($('#rocket-img'), $('.moon'))
+  $('#rocket-box').show()
+  $('#rocket-box').slideUp(2000)
+  collision($('#rocket-img'), $('.moon'))
 });
 
+var score = 0;
+
+
  function collision($div1, $div2) {
-      var x1 = $div1.offset().left;
-      var y1 = $div1.offset().top;
-      var h1 = $div1.outerHeight(true);
-      var w1 = $div1.outerWidth(true);
-      var b1 = y1 + h1;
-      var r1 = x1 + w1;
-      var x2 = $div2.offset().left;
-      var y2 = $div2.offset().top;
-      var h2 = $div2.outerHeight(true);
-      var w2 = $div2.outerWidth(true);
-      var b2 = y2 + h2;
-      var r2 = x2 + w2;
+    var x1 = $div1.offset().left;
+    var y1 = $div1.offset().top;
+    var h1 = $div1.outerHeight(true);
+    var w1 = $div1.outerWidth(true);
+    var b1 = y1 + h1;
+    var r1 = x1 + w1;
+    var x2 = $div2.offset().left;
+    var y2 = $div2.offset().top;
+    var h2 = $div2.outerHeight(true);
+    var w2 = $div2.outerWidth(true);
+    var b2 = y2 + h2;
+    var r2 = x2 + w2;
 
-      if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
-      return alert('You SHOT THE MOON');
-    }
+    if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
+    return success();
+}
 
-
-
+function success () {
+    score += 1;
+    $('#score').html(score);
+}
